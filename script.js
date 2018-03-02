239f66a62afaf570cec82caa171d94d901d3159b
Liane Membis <lmembis@gmail.com>
2018-03-01 21:27:07 -0500

    Completed user story for adding ending to game

--- script.js
+++ script.js
@@ -168,7 +168,18 @@ function onKeyDown(k){
   	if(player.y <= 240){
   		player.move(player.speed);
   	}
-  }
+};
+
+if(k.keyCode === 32){
+    if(gameStart === false){
+      startScreen.style.display = "none";
+      canvas.style.visibility = "visible";
+      playerScoreboard.style.visibility = "visible";
+      compScoreboard.style.visibility = "visible";
+      gameStart = true;
+      step();
+    };
+  };
 };
 
 //Listen for Event(Keypress)
@@ -176,20 +187,34 @@ function addKeyEvents(){
 	window.addEventListener('keydown', onKeyDown, true);
 };
 
+var gameStart = false;
 var player = new Paddle(10, 130, 10, 50);
 var computer = new Paddle(480, 130, 10, 50);
 var ball = new Ball();
 var playerScore = 0;
 var compScore = 0;
 
+var startScreen = document.getElementById("intro");
 var playerScoreboard = document.getElementById("player-score");
 var compScoreboard = document.getElementById("comp-score");
+var endGame = document.getElementById("game-end");
 
-var update = function(){
-  ball.move(player, computer);
-  computer.computerUpdate(ball);
-  playerScoreboard.innerHTML = "Score: " + playerScore;
-  compScoreboard.innerHTML = "Score: " + compScore;
+
+//Game End
+function gameEnd(){
+  if(playerScore === 1){
+    canvas.style.display = "none";
+    playerScoreboard.style.display = "none";
+    compScoreboard.style.display = "none";
+    endGame.parentNode.style.display = "block";
+    endGame.innerHTML = "YASSS QUEEN YOU WIN!"
+  } else if(compScore === 1){
+    canvas.style.display = "none";
+    playerScoreboard.style.display = "none";
+    compScoreboard.style.display = "none";
+    endGame.parentNode.style.display = "block";
+    endGame.innerHTML = "SORRY BOO YOU LOSE!"
+  };
 };
 
 function render(){
@@ -197,6 +222,14 @@ function render(){
   player.render();
   computer.render();
   ball.render();
+};
+
+var update = function(){
+  ball.move(player, computer);
+  computer.computerUpdate(ball);
+  playerScoreboard.innerHTML = "Score: " + playerScore;
+  compScoreboard.innerHTML = "Score: " + compScore;
+  gameEnd();
 };
 
 //Animation
@@ -210,10 +243,11 @@ var animate = window.requestAnimationFrame ||
 	};
 
 function step(){
-  update();
-  render();
-  //Initialize Animation
-  animate(step);
+  if(gameStart === true){
+    update();
+    render();
+    animate(step);
+  };
 };
 
 //Load everything!
